02f4ba39af4aac45729b58ba4a89b8e66d2b7e9d
WaywardKoyote <117386123+WaywardKoyote@users.noreply.github.com>
2026-09-24 15:57:20 -0500

    Minor Cam Controller Update

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,15 +5,23 @@ public class CameraController : MonoBehaviour
     public GridManager gridManager;
     public float rotSpeed = 5f;
 
+    private Vector3 rotPoint;
+    private float camInput;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rotPoint = gridManager.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.transform.RotateAround(rotPoint, Vector3.up, rotSpeed * camInput * Time.deltaTime);
+    }
+
+    public void OnMove(Vector2 value)
+    {
+        camInput = value.x;
     }
 }
